70290850b9f546c43b93df4e4f4d709530a9d0f4
Abhijit Hoskeri <abhijithoskeri@gmail.com>
2019-03-21 13:37:59 -0700

    initial commit

--- reqthing.go
+++ reqthing.go
@@ -1,15 +1,38 @@
 package reqthing
 
 import "log"
-import "net/url"
 import "net/http"
 
 var Info log.Logger
 var Debug log.Logger
 
 type Reqthing struct {
-	name      string
-	base      *url.URL
-	tr        *http.Transport
-	cachePath string
+	tr         *http.Transport
+	name       string
+	cacheDir   string
+	hashSecret []byte
+}
+
+type Options struct {
+	CacheDir   string
+	Name       string
+	HashSecret string
+}
+
+// get cache path for a url
+func (r *Reqthing) cacheId(url string) (string, error) {
+	// normalize the url.
+	// calculate hmac
+	// filepath/join to the cache dir
+	return "", nil
+}
+
+// create a new Reqthing
+func New(options Options) *Reqthing {
+	return &Reqthing{}
+}
+
+// perform an http request
+func (r *Reqthing) Roundtrip(req *http.Request) (*http.Response, error) {
+	return nil, nil
 }
